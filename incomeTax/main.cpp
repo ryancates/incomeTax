9dbab094671732f0c99c5ac89e6aa0383d41d945
Ryan Cates <ryan@macbook-ryno.gateway.2wire.net>
2011-04-08 15:10:20 -0700

    Initial Commit

---
+++ incomeTax/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  incomeTax
+//
+//  Created by Ryan Cates on 4/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
